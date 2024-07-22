13c5be41732436eed06b3f7c030de18ce2fe3b01
fikarsuwardi <fikarsuwardi@gmail.com>
2024-07-23 01:17:19 +0700

    update cv

---
+++ script.js
@@ -0,0 +1,244 @@
+const listProjectMobile = [
+  {
+    id: 1,
+    name: "Kasir Super",
+    img1: "images/project/kasir/beranda.png",
+    img2: "images/project/kasir/produk.png",
+    img3: "images/project/kasir/transaksi.png",
+    img4: "images/project/kasir/lainnya.png",
+    desc: "A cashier application is software used by businesses to process sales transactions, manage inventory, and generate sales reports efficiently.",
+    repo: "https://github.com/fikarsuwardi/my_kasir_super",
+    link: "https://kasirsuper-demo.vercel.app/",
+    techStack: "Flutter, Firebase, Bloc, Dio, shared_preference",
+  },
+  {
+    id: 2,
+    name: "Vesperia App",
+    img1: "images/project/vesperia/login.png",
+    img2: "images/project/vesperia/detail-produk.png",
+    img3: "images/project/vesperia/edit-profile.png",
+    img4: "images/project/vesperia/list-product.png",
+    desc: "A commerce application that show list of shopping item",
+    repo: "https://github.com/fikarsuwardi/vesperia-flutter-app",
+    link: "",
+    techStack: "Flutter, GetX, Dio, GetStorage",
+  },
+  {
+    id: 3,
+    name: "Expenses App",
+    img1: "images/project/expenses/all.png",
+    img2: "images/project/expenses/add.png",
+    img3: "images/project/expenses/edit.png",
+    img4: "images/project/expenses/recent.png",
+    desc: "Application to record every expense",
+    repo: "https://github.com/fikarsuwardi/expenses_app",
+    link: "",
+    techStack: "React Native, Expo, Axios, Realtime Database",
+  },
+  {
+    id: 4,
+    name: "Flash Chat",
+    img1: "images/project/flashchat/chat.png",
+    img2: "images/project/flashchat/login.png",
+    img3: "images/project/flashchat/register.png",
+    img4: "images/project/flashchat/chat.png",
+    desc: "A chat application with registration, login and chat features",
+    repo: "https://github.com/fikarsuwardi/Flash-Chat-IOS",
+    link: "",
+    techStack: "Swift, Xcode, CocoaPods, Authentication, Cloud Database, MVC",
+  },
+];
+const listProjectWeb = [
+  {
+    id: 1,
+    name: "CMS Restaurant App Server",
+    img1: "images/project/restoVue/server.png",
+    img2: "images/project/restoVue/add.png",
+    img3: "images/project/restoVue/category.png",
+    img4: "images/project/restoVue/history.png",
+    desc: "A web application that show list food.",
+    repo: "https://github.com/fikarsuwardi/restaurant_app_server",
+    link: "",
+    techStack:
+      "SQL, Node, Express, PostgreSQL, Sequelize, Axios, jsonwebtoken, cors, dotenv, google-auth-library",
+  },
+  {
+    id: 2,
+    name: "CMS Restaurant App Client JQuery",
+    img1: "images/project/restoJquery/list.png",
+    img2: "images/project/restoJquery/add.png",
+    img3: "images/project/restoJquery/category.png",
+    img4: "images/project/restoJquery/login.png",
+    desc: "A web application that show list food.",
+    repo: "https://github.com/fikarsuwardi/restaurant_app_client_jquery",
+    link: "",
+    techStack: "HTML, CSS, JQuery Ajax",
+  },
+  {
+    id: 3,
+    name: "CMS Restaurant App Client Vue",
+    img1: "images/project/restoVue/list.png",
+    img2: "images/project/restoVue/add.png",
+    img3: "images/project/restoVue/category.png",
+    img4: "images/project/restoVue/history.png",
+    desc: "A web application that show list food.",
+    repo: "https://github.com/fikarsuwardi/cms-restaurant-app-client-vue-router",
+    link: "",
+    techStack: "Vue, Vue Router, Axios, Pinia",
+  },
+  {
+    id: 4,
+    name: "Movie List",
+    img1: "images/project/movieList/movieFikar.jpeg",
+    img2: "images/project/movieList/detailMovie.png",
+    img3: "images/project/movieList/listActor.png",
+    img4: "images/project/movieList/detailActor.png",
+    desc: "A web application that show list of popular movie",
+    repo: "https://github.com/fikarsuwardi/movie-fikar-vue",
+    link: "https://movie-fikar-vue.web.app/",
+    techStack: "Vue, Pinia, Tailwind",
+  },
+  {
+    id: 5,
+    name: "Book Review",
+    img1: "images/project/bookReview/list.png",
+    img2: "images/project/bookReview/detail.png",
+    img3: "images/project/bookReview/add.png",
+    img4: "images/project/bookReview/search.png",
+    desc: "A web application that show list of books with review and rating feature.",
+    repo: "https://github.com/fikarsuwardi/l11-book-review",
+    link: "",
+    techStack: "Laravel",
+  },
+  {
+    id: 6,
+    name: "Online Shop CMS Server Side",
+    img1: "images/project/fikarShop/api.png",
+    img2: "images/project/fikarShop/add.png",
+    img3: "images/project/fikarShop/images.png",
+    img4: "images/project/fikarShop/register.png",
+    desc: "A web application that show list of selling item.",
+    repo: "https://github.com/fikarsuwardi/fikarShop-server",
+    link: "",
+    techStack:
+      "Node, Express, PostgreSQL, Sequelize, Axios, jsonwebtoken, cors, dotenv",
+  },
+  {
+    id: 7,
+    name: "Online Shop CMS Admin Side",
+    img1: "images/project/fikarShop/list.png",
+    img2: "images/project/fikarShop/add.png",
+    img3: "images/project/fikarShop/images.png",
+    img4: "images/project/fikarShop/register.png",
+    desc: "A web application that show list of selling item.",
+    repo: "https://github.com/fikarsuwardi/fikarShop-admin-side",
+    link: "",
+    techStack: "React, redux, router, bootstrap",
+  },
+  {
+    id: 8,
+    name: "Personal Finance App",
+    img1: "images/project/finance/reports.png",
+    img2: "images/project/finance/add.png",
+    img3: "images/project/finance/home.png",
+    img4: "images/project/finance/transaction.png",
+    desc: "The Personal Finance App is designed to help you manage your finances effectively.",
+    repo: "https://github.com/fikarsuwardi/personal_finance_app",
+    link: "",
+    techStack: "React, router, tailwind",
+  },
+  
+];
+
+function loadProjectsMobile() {
+  const projectContainer = document.getElementById("project-cards-mobile");
+
+  listProjectMobile.forEach((project) => {
+    const projectCard = document.createElement("div");
+    projectCard.classList.add("col-md-4", "mb-4");
+    projectCard.innerHTML = `
+          <div class="card shadow pt-3">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-12">
+                        <img src="${project.img1}" data-src="${project.img1}" alt="${project.name}" class="img-thumbnail center rounded mx-auto d-block half-height thumbnail" data-toggle="modal" data-target="#imageModal" />
+                      </div>
+                    </div>
+                    </br>
+                    <div class="row">
+                      <div class="col-6 col-md-4">
+                        <img src="${project.img2}" data-src="${project.img2}" alt="${project.name}" class="img-thumbnail center rounded mx-auto d-block thumbnail" data-toggle="modal" data-target="#imageModal" />
+                      </div>
+                      <div class="col-6 col-md-4">
+                        <img src="${project.img3}" data-src="${project.img3}" alt="${project.name}" class="img-thumbnail center rounded mx-auto d-block thumbnail" data-toggle="modal" data-target="#imageModal" />
+                      </div>
+                      <div class="col-6 col-md-4">
+                        <img src="${project.img4}" data-src="${project.img4}" alt="${project.name}" class="img-thumbnail center rounded mx-auto d-block thumbnail" data-toggle="modal" data-target="#imageModal" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                      <h5 class="card-title">${project.name}</h5>
+                      <p class="card-text">${project.desc}</p>
+                      <h6 class="card-subtitle mb-2 text-muted">Tech Stack :</h6>
+                      <p class="card-text">${project.techStack}</p>
+                      <a href="${project.repo}" class="btn btn-primary">GitHub</a>
+                      <a href="${project.link}" class="btn btn-secondary">Website</a>
+                  </div>
+          </div>
+      `;
+    projectContainer.appendChild(projectCard);
+  });
+}
+
+function loadProjectsWeb() {
+  const projectContainer = document.getElementById("project-cards-web");
+
+  listProjectWeb.forEach((project) => {
+    const projectCard = document.createElement("div");
+    projectCard.classList.add("col-md-4", "mb-4");
+    projectCard.innerHTML = `
+          <div class="card shadow pt-3">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-12">
+                        <img src="${project.img1}" data-src="${project.img1}" alt="${project.name}" class="img-thumbnail center rounded mx-auto d-block half-height thumbnail" data-toggle="modal" data-target="#imageModal" />
+                      </div>
+                    </div>
+                    </br>
+                    <div class="row">
+                      <div class="col-6 col-md-4">
+                        <img src="${project.img2}" data-src="${project.img2}" alt="${project.name}" class="img-thumbnail center rounded mx-auto d-block thumbnail" data-toggle="modal" data-target="#imageModal" />
+                      </div>
+                      <div class="col-6 col-md-4">
+                        <img src="${project.img3}" data-src="${project.img3}" alt="${project.name}" class="img-thumbnail center rounded mx-auto d-block thumbnail" data-toggle="modal" data-target="#imageModal" />
+                      </div>
+                      <div class="col-6 col-md-4">
+                        <img src="${project.img4}" data-src="${project.img4}" alt="${project.name}" class="img-thumbnail center rounded mx-auto d-block thumbnail" data-toggle="modal" data-target="#imageModal" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                      <h5 class="card-title">${project.name}</h5>
+                      <p class="card-text">${project.desc}</p>
+                      <h6 class="card-subtitle mb-2 text-muted">Tech Stack :</h6>
+                      <p class="card-text">${project.techStack}</p>
+                      <a href="${project.repo}" class="btn btn-primary">GitHub</a>
+                      <a href="${project.link}" class="btn btn-secondary">Website</a>
+                  </div>
+          </div>
+      `;
+    projectContainer.appendChild(projectCard);
+  });
+}
+
+// Call the function to load projects
+loadProjectsMobile();
+loadProjectsWeb();
+
+$('#imageModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget);
+  var src = button.data('src');
+  var modal = $(this);
+  modal.find('#modalImage').attr('src', src);
+});
